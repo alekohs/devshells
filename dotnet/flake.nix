0e38f2008f4cf048cb14c0a4d7c7b9841c2f9b3e
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-02-17 07:45:35 +0100

    Added dotnet and nodejs shells (#1)
    * Added dotnet shell
    
    * Added node and readme file

---
+++ dotnet/flake.nix
@@ -0,0 +1,20 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs";
+        utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, utils }:
+        utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+            in
+                {
+                devShell = with pkgs; mkShell {
+                    buildInputs = [
+                        dotnetCorePackages.sdk_9_0
+                    ];
+                };
+            }
+        );
+}
