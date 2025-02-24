4d4b60fc9b6ba4a57452359b9888e6e225f5f064
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-02-24 14:54:16 +0100

    Combined rust and rustup to same devshell

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/92d295f588631b0db2da509f381b4fb1e74173c5";
+        nixpkgs.url = "github:NixOS/nixpkgs";
         utils.url = "github:numtide/flake-utils";
     };
 
@@ -10,19 +10,28 @@
                 pkgs = import nixpkgs { inherit system; };
             in
                 {
-                devShell = with pkgs; mkShell {
-                    buildInputs = [
-                        darwin.apple_sdk.frameworks.Security
-                        libiconv
-                        gcc
-                        cargo
-                        rustc
-                        rustfmt
-                        rustPackages.clippy
-                        rust-analyzer
-                    ];
+                devShells = {
+                    rust = with pkgs; mkShell {
+                        buildInputs = [
+                            libiconv
+                            gcc
+                            cargo
+                            rustc
+                            rustfmt
+                            rustPackages.clippy
+                            rust-analyzer
+                        ] ++ (if system == "aarch64-darwin" then [ darwin.apple_sdk.frameworks.Security ] else []);
 
-                    RUST_SRC_PATH = rustPlatform.rustLibSrc;
+                        RUST_SRC_PATH = rustPlatform.rustLibSrc;
+                    };
+
+                    default = with pkgs; mkShell {
+                        buildInputs = [
+                            libiconv
+                            gcc
+                            rustup
+                        ] ++ (if system == "aarch64-darwin" then [ darwin.apple_sdk.frameworks.Security ] else []);
+                    };
                 };
             }
         );
