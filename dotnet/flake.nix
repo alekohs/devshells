5ecb2c595dedb046698a7379d0ee7c46f4b5665b
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-02-24 20:49:35 +0100

    Added ef core tools to dotnet

--- dotnet/flake.nix
+++ dotnet/flake.nix
@@ -8,25 +8,28 @@
         utils.lib.eachDefaultSystem (system:
             let
                 pkgs = import nixpkgs { inherit system; };
+                hook = ''
+                    echo 'Installing EF core tools'
+                    dotnet tool install --global dotnet-ef
+                '';
             in
                 {
                 devShells = {
                     dotnet8 = with pkgs; mkShell {
-                        packages = [
-                        ];
+                        packages = [];
 
                         buildInputs = [
                             dotnetCorePackages.sdk_8_0
                         ];
+                        shellHook = hook;
                     };
 
                     default = with pkgs; mkShell {
-                        packages = [
-                        ];
-
+                        packages = [];
                         buildInputs = [
                             dotnetCorePackages.sdk_9_0
                         ];
+                        shellHook = hook;
                     };
 
                 };
