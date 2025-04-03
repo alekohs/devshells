e655040f72bb13e0f21d2747f11da897d41020c7
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-04-03 08:15:52 +0200

    Updated nodejs

--- nodejs/flake.nix
+++ nodejs/flake.nix
@@ -1,44 +1,64 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs";
-        utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-    outputs = { self, nixpkgs, utils }:
-        utils.lib.eachDefaultSystem (system:
-            let
-                pkgs = import nixpkgs { inherit system; };
-            in
-                {
-                devShells = {
-                    pnpm = with pkgs; mkShell {
-                        name = "Pnpm dev shell";
-                        buildInputs = [
-                            nodejs_22
-                            nodePackages.prettier
-                            nodePackages.tailwindcss
-                            nodePackages.pnpm
-                        ];
-                    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        sharedPackages = [
+          pkgs.nodejs_22
+          pkgs.nodePackages.prettier
+        ];
+        hook = ''
+          echo "Node $(node -v)"
+        '';
+      in
+      {
+        devShells = {
+          pnpm =
+            with pkgs;
+            mkShell {
+              name = "Pnpm dev shell";
+              packages = [
+                nodePackages.tailwindcss
+                nodePackages.pnpm
+              ] ++ sharedPackages;
+              shellHook = ''
+                  echo "Welcome to the dev shell for NodeJS and PNPM"
+                '' + hook;
+            };
 
-                    yarn = with pkgs; mkShell {
-                        name = "Yarn dev shell";
-                        buildInputs = [
-                            nodejs_22
-                            yarn
-                        ];
-                    };
+          yarn =
+            with pkgs;
+            mkShell {
+              name = "Yarn dev shell";
+              packages = [ yarn ] ++ sharedPackages;
+              shellHook = ''
+                  echo "Welcome to the dev shell for NodeJS and Yarn"
+                '' + hook;
 
+            };
 
-                    default = with pkgs; mkShell {
-                        name = "Node dev shell";
-                        buildInputs = [
-                            nodejs_22
-                            nodePackages.prettier
-                            nodePackages.tailwindcss
-                        ];
-                    };
-                };
-            }
-        );
+          default =
+            with pkgs;
+            mkShell {
+              name = "Node dev shell";
+              packages = sharedPackages;
+              shellHook = ''
+                  echo "Welcome to the dev shell for NodeJS"
+                '' + hook;
+
+            };
+        };
+      }
+    );
 }
